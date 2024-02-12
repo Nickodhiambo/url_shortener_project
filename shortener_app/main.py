bf4e5475f82b39b5292021bc296d94accc82381d
Nick <nodhimabo01@gmail.com>
2024-02-12 10:47:33 +0300

    Implement URL deactivation

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -80,3 +80,18 @@ def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
     db_url.url = str(base_url.replace(path=db_url.key))
     db_url.admin_url = str(base_url.replace(path=admin_endpoint))
     return db_url
+
+@app.delete("/admin/{secret_key}")
+def delete_url(
+        secret_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+        ):
+    """An endpoint to deactivate a shortened URL
+    by its secret key"""
+    if db_url := crud.deactivate_db_url_by_secret_key(db, secret_key=secret_key):
+        message = f"Successfully deactivated shortened URL for '{db_url.target_url}'"
+        return {"detail": message}
+    else:
+        raise_not_found(request)
+

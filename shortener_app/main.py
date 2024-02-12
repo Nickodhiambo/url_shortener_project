983e5a5fccbf6c2ac28d0c73c4b8b1f3e3cfd2b3
Nick <nodhimabo01@gmail.com>
2024-02-12 09:47:30 +0300

    Modify create url endpoint

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import Session
 from . import models, schemas, crud
 from .database import SessionLocal, engine
 
+from starlette.datastructures import URL
+from .config import get_settings
+
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
@@ -50,7 +53,29 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
         raise_bad_request(message="Your provided URL is not valid")
     
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
+    return get_admin_info(db_url)
 
+@app.get(
+        "/admin/{secret_key}",
+        name="admin info",
+        response_model=schemas.URLInfo
+        )
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    """Sends a get request to admin endpoint with secret_key as param"""
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
+
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    """Concatenates the returned shortened and admin keys
+    to base URL
+    """
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+            "admin info", secret_key=db_url.secret_key
+            )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
     return db_url
